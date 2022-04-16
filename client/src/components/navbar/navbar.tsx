c4581a43f3e517f9241e3e199dca56b80f39c2bf
MichaelLin12 <michael.lin2032@gmail.com>
2022-04-16 16:26:28 -0500

    work on navbar

--- client/src/components/navbar/navbar.tsx
+++ client/src/components/navbar/navbar.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './navbar.css'
 import {Link} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faUser} from "@fortawesome/free-solid-svg-icons"
+import {faUser, faBars} from "@fortawesome/free-solid-svg-icons"
 import logo from '../../images/EdensGarden.png'
+import {Popover} from '@mui/material';
 
 
 export default function Navbar() {
+  const [displayHamburger, setDisplayHamburger] = useState(false);
   return (
     <header className='header'>
       <span className='logo'><img src={logo} alt="Eden's Garden" height={50} width={50}/></span>
+      <div className='parent'>
+        <button className='hamburger' onClick={()=>{displayHamburger? setDisplayHamburger(false): setDisplayHamburger(true)}}>
+          <FontAwesomeIcon icon={faBars}/>
+        </button>
+        <div className='menu' style={{display: displayHamburger? 'block':'none'}}>
+          Hello
+        </div>
+      </div>
       <nav className='navbar'>
         <ul>
           <li><Link to='/store'>Store</Link></li>
