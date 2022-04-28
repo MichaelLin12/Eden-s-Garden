3bf4b73b352dc0f32c0666dc33c219b2edaed8ba
MichaelLin12 <michael.lin2032@gmail.com>
2022-04-28 15:00:42 -0500

    work on support page

--- client/src/components/donate/Donate.tsx
+++ client/src/components/donate/Donate.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import './donate.css'
+import img1 from '../../images/istockphoto-471500478-612x612.jpg'
 
 export default function Donate() {
   return (
-    <div>Donate</div>
+    <div className='donate'>
+      <div className='donate-grid'>
+        <div className='donate-grid-1'>
+          <img src={img1} alt='sensitive' width={400}/>
+          <div className='donate-content'>
+            Ex ipsum veniam proident incididunt in pariatur commodo ipsum aliqua ea. Adipisicing est id deserunt dolor laborum mollit amet esse voluptate consectetur est labore eu ipsum. Voluptate elit sunt ullamco aliquip minim incididunt tempor mollit proident incididunt. Dolore incididunt tempor qui sint aute anim. Dolor id velit duis ad in ex eu cillum.
+          </div>
+        </div>
+        <div className='donate-grid-2'>
+          <div className='donate-2-title'>Donate</div>
+          <div className='donate-2-suggestions'>
+            <button>$5</button>
+            <button>$10</button>
+            <button>$20</button>
+            <button>$50</button>
+            <button>$100</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
